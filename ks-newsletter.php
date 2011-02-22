ab2045a55fc80048f1f4679099b79c2621858755
Mat_ <mathieu@kune-studio.com>
2011-02-22 11:29:16 +0100

    First commit

---
+++ ks-newsletter.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: KS Newsletter
+Plugin URI: http://www.kune-studio.com
+Description: Un gestionnaire de newsletter simple.
+Version: 0.1
+Author: Mathieu Basili
+Author URI: http://www.kune-studio.com
+License: GPL2
+*/
+?>
